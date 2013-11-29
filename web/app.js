1898899c4c0a38b2013791f4e59849b912d42561
Sam Agnew <sagnew@eden.rutgers.edu>
2013-11-28 21:29:10 -0500

    Created basic html headers and footers

--- web/app.js
+++ web/app.js
@@ -5,14 +5,20 @@ var freefood = require('./freefood')();
 function printEvents(events){
     var i;
     for(i = 0; i < events.length; i++){
-        console.log(events[i]);
+        console.log(events[i]['foodWord'] + "\n");
+        console.log(events[i]['description'][0] + "\n");
     }
 }
+
+//Set up ejs for rendering
+app.engine('.html', require('ejs').__express;
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 
 freefood.getFreeFoodEvents(printEvents);
 
 app.get('/', function(req, res){
-    res.send('Free food!');
+    res.render('index', { events: events });
 });
 
 app.listen(3000);
