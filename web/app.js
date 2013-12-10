a6b916d858c46ebe33427a9f20a8524b3bfe4247
Sam Agnew <sagnew@eden.rutgers.edu>
2013-12-10 05:25:01 -0500

    Removed unnecessary testing code

--- web/app.js
+++ web/app.js
@@ -2,22 +2,11 @@ var express = require('express');
 var app = express();
 var freefood = require('./freefood')();
 
-function printEvents(events){
-    var i;
-    for(i = 0; i < events.length; i++){
-        console.log(events[i].title + "\n" +
-                events[i].foodWord + "\n" +
-                events[i]['when'] + "\n");
-    }
-}
-
 //Set up ejs for rendering
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/static'));
-
-freefood.getFreeFoodEvents(printEvents);
 
 app.get('/', function(req, res){
     freefood.getFreeFoodEvents(function(events){
